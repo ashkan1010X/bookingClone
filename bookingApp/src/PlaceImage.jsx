615fdf6f7145e64df81b0e5470b0564479fa15b8
ashkan1010X <aasadpour@myseneca.ca>
2025-01-27 18:43:23 -0500

    sdsds

--- bookingApp/src/PlaceImage.jsx
+++ bookingApp/src/PlaceImage.jsx
@@ -2,18 +2,19 @@ export default function PlaceImage({ place, idx = 0, className = null }) {
   if (!className) {
     className = "aspect-square object-cover rounded-lg";
   }
+
   const baseURL =
     window.location.hostname === "localhost"
       ? "http://localhost:5000/"
-      : "https://bookingclone-backend-5pei.onrender.com";
+      : "https://bookingclone-backend-5pei.onrender.com/";
 
-  const photoURL = place.addedPhotos[idx].startsWith("uploads/")
+  const imagePath = place.addedPhotos[idx].startsWith("uploads/")
     ? place.addedPhotos[idx]
     : `uploads/${place.addedPhotos[idx]}`;
 
   return (
     <div>
-      <img className={className} src={`${baseURL}/${photoURL}`} alt="" />
+      <img className={className} src={`${baseURL}/${imagePath}`} alt="" />
     </div>
   );
 }
