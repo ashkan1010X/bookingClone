105be8566fdc9697fd1fc29bc34d14b3438d0f41
ashkan1010X <aasadpour@myseneca.ca>
2025-01-27 18:38:15 -0500

    sdsd

--- bookingApp/src/PlaceImage.jsx
+++ bookingApp/src/PlaceImage.jsx
@@ -4,7 +4,7 @@ export default function PlaceImage({ place, idx = 0, className = null }) {
   }
   const baseURL =
     window.location.hostname === "localhost"
-      ? "http://localhost:5000/"
+      ? "http://localhost:5000"
       : "https://bookingclone-backend-5pei.onrender.com";
 
   const photoURL = place.addedPhotos[idx].startsWith("uploads/")
